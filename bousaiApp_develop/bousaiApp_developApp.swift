716d57d3aa4783d81fde0a8b01dbd5b2ee8757e8
kazamarain <94329696+kazamarain@users.noreply.github.com>
2023-08-02 18:55:15 +0900

    Initial Commit

---
+++ bousaiApp_develop/bousaiApp_developApp.swift
@@ -0,0 +1,17 @@
+//
+//  bousaiApp_developApp.swift
+//  bousaiApp_develop
+//
+//  Created by ユウ・カザマ on 2023/08/02.
+//
+
+import SwiftUI
+
+@main
+struct bousaiApp_developApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
